9afea886420f6c589c9e3f782d37f370b550054f
Espen Wobbes <espen.wobbes@nhn.no>
2025-10-31 14:27:33 +0100

    wrapper for time.Duration for better json unmarshal/marshal

---
+++ internal/utils/timesutil/duration.go
@@ -0,0 +1,36 @@
+// wrapper that embeds time.Duration to make the json package able to marshal a string representation of duration
+// to an actual object of type time.Duration
+// (for example the json key:value "duration": "5s", would be parsed to a duration of time.Second * 5)
+package timesutil
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type Duration time.Duration
+
+func (d Duration) MarshalJSON() ([]byte, error) {
+	return json.Marshal(d.String())
+}
+
+func (d *Duration) UnmarshalJSON(b []byte) error {
+	var s string
+	err := json.Unmarshal(b, &s)
+	if err != nil {
+		return err
+	}
+
+	parsed, err := time.ParseDuration(s)
+	if err == nil {
+		*d = Duration(parsed)
+		return nil
+	}
+
+	return fmt.Errorf("failed to parse duration from JSON: %s", string(b))
+}
+
+func (d Duration) String() string {
+	return time.Duration(d).String()
+}
