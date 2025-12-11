6073104e50d062428f96fb7ddb2a2e0369649fb8
Espen Wobbes <espen.wobbes@nhn.no>
2025-12-11 14:12:30 +0100

    fix: (dns/handler) registers gslb-config with fqdn as the record name

--- internal/dns/handler.go
+++ internal/dns/handler.go
@@ -119,11 +119,10 @@ func (h *Handler) handleRecord(record dns.RR) *service.Service {
 	if !ok {
 		return nil
 	}
-	h.log.Debug(txt.Hdr.Name)
 
 	rawData := txt.Txt[0]
 	data := strings.ReplaceAll(rawData, "\\", "")
-	svcConfig := model.GSLBConfig{}
+	svcConfig := model.GSLBConfig{Fqdn: txt.Hdr.Name}
 	err := json.Unmarshal([]byte(data), &svcConfig)
 	if err != nil {
 		h.log.Errorf("failed to parse gslb config: %v", err.Error())
