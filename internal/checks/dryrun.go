fe3d7aa8e641248c261d7e826c30c9ceb967046e
Espen Wobbes <espen.wobbes@nhn.no>
2026-02-27 09:22:05 +0100

    fix(worker-pool): fixed locking when checking blocked queue

--- internal/checks/dryrun.go
+++ internal/checks/dryrun.go
@@ -9,6 +9,9 @@ import (
 type DryRun struct{}
 
 func (dr *DryRun) Check() error {
+
+	sleepDuration := time.Duration(100+rand.Intn(400)) * time.Millisecond
+	time.Sleep(sleepDuration)
 	num := rand.Intn(10)
 	if num == 0 { // 10% failure when dryrunning
 		return errors.New("dry-run fail")
