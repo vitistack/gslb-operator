2488e06616be1d738fb6c29023dedb9e28dcb7b5
Espen Wobbes <espen.wobbes@nhn.no>
2026-06-23 14:04:49 +0200

    refactor models for dualstack support

--- pkg/models/spoofs/override.go
+++ pkg/models/spoofs/override.go
@@ -1,10 +1,35 @@
 package spoofs
 
 import (
-	"net/netip"
+	"encoding/json"
+	"fmt"
+
+	"github.com/vitistack/gslb-operator/internal/utils/ip"
 )
 
 type Override struct {
 	MemberOf string     `json:"memberOf"`
-	IP       netip.Addr `json:"ip"`
+	Address  ip.Address `json:"address"`
+}
+
+func (o *Override) UnmarshalJSON(b []byte) error {
+	type Alias Override
+	aux := struct {
+		Address json.RawMessage `json:"address"`
+		*Alias
+	}{
+		Alias: (*Alias)(o),
+	}
+
+	if err := json.Unmarshal(b, &aux); err != nil {
+		return err
+	}
+
+	addr, err := ip.ParseAddressJSON(aux.Address)
+	if err != nil {
+		return fmt.Errorf("address: %w", err)
+	}
+	o.Address = addr
+
+	return nil
 }
--- pkg/models/spoofs/spoofs.go
+++ pkg/models/spoofs/spoofs.go
@@ -1,8 +1,37 @@
 package spoofs
 
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/vitistack/gslb-operator/internal/utils/ip"
+)
+
 type Spoof struct {
-	Name string `json:"name"`
-	UUID string `json:"uuid"`
-	FQDN string `json:"fqdn"`
-	IP   string `json:"ip"`
+	Name    string     `json:"name"`
+	UUID    string     `json:"uuid"`
+	FQDN    string     `json:"fqdn"`
+	Address ip.Address `json:"address"`
+}
+
+func (s *Spoof) UnmarshalJSON(b []byte) error {
+	type Alias Spoof
+	aux := struct {
+		Address json.RawMessage `json:"address"`
+		*Alias
+	}{
+		Alias: (*Alias)(s),
+	}
+
+	if err := json.Unmarshal(b, &aux); err != nil {
+		return err
+	}
+
+	addr, err := ip.ParseAddressJSON(aux.Address)
+	if err != nil {
+		return fmt.Errorf("address: %w", err)
+	}
+	s.Address = addr
+
+	return nil
 }
