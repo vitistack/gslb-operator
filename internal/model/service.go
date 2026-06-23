c4a6cf682a06105036ceb9d80e27e8f4f3004c6a
Espen Wobbes <espen.wobbes@nhn.no>
2026-06-23 14:03:07 +0200

    refactor model objects for dualstack support

--- internal/model/service.go
+++ internal/model/service.go
@@ -1,10 +1,11 @@
 package model
 
 import (
-	"net"
-	"net/netip"
+	"encoding/json"
+	"fmt"
 
 	"github.com/google/uuid"
+	"github.com/vitistack/gslb-operator/internal/utils/ip"
 	"github.com/vitistack/gslb-operator/pkg/models/spoofs"
 )
 
@@ -37,9 +38,13 @@ func (g GSLBServiceGroup) Spoof() *spoofs.Spoof {
 
 	active, ok := g.Members[g.Active]
 	if ok {
-		spoof.IP = active.IP.String()
+		spoof.Address = active.Address
 	} else if g.HasOverride {
-		spoof.IP = net.ParseIP(g.Active).String()
+		addr, err := ip.FromString(g.Active)
+		if err != nil {
+			spoof.Address = nil
+		}
+		spoof.Address = addr
 	}
 
 	return &spoof
@@ -51,12 +56,31 @@ type GSLBService struct {
 	ID           string     `json:"id"`
 	MemberOf     string     `json:"memberOf"`
 	Fqdn         string     `json:"fqdn"`
+	Port         string     `json:"port"`
 	Datacenter   string     `json:"datacenter"`
-	IP           netip.Addr `json:"ip"`
+	Address      ip.Address `json:"address"`
 	IsHealthy    bool       `json:"isHealthy"`
 	FailureCount int        `json:"failureCount"`
 }
 
-func (s GSLBService) Key() string {
-	return s.MemberOf
+func (s *GSLBService) UnmarshalJSON(b []byte) error {
+	type Alias GSLBService
+	aux := struct {
+		Address json.RawMessage `json:"address"`
+		*Alias
+	}{
+		Alias: (*Alias)(s),
+	}
+
+	if err := json.Unmarshal(b, &aux); err != nil {
+		return err
+	}
+
+	addr, err := ip.ParseAddressJSON(aux.Address)
+	if err != nil {
+		return fmt.Errorf("address: %w", err)
+	}
+	s.Address = addr
+
+	return nil
 }
