ba5837022679c51efe3f81c4449d5980bf669d0c
Espen Wobbes <espen.wobbes@nhn.no>
2026-08-03 10:46:50 +0200

    fix: prometheus metrics for dnsdist servers on sync-job

--- internal/dns/update/dnsdist/dnsdist.go
+++ internal/dns/update/dnsdist/dnsdist.go
@@ -98,11 +98,7 @@ func (d *DNSDISTUpdater) Create(records ...update.Record) error {
 
 	for _, server := range d.servers {
 		wg.Go(func() error {
-			err := d.do(server.name,
-				func() error {
-					return server.Create(records...)
-				},
-			)
+			err := d.do(server.name, func() error { return server.Create(records...) })
 			if err != nil {
 				return err
 			}
@@ -138,12 +134,7 @@ func (d *DNSDISTUpdater) Delete(id string, views ...string) error {
 
 	for _, server := range d.servers {
 		wg.Go(func() error {
-			return d.do(
-				server.name,
-				func() error {
-					return server.Delete(id, views...)
-				},
-			)
+			return d.do(server.name, func() error { return server.Delete(id, views...) })
 		})
 	}
 
@@ -208,38 +199,6 @@ func (d *DNSDISTUpdater) Synchronize(ctx context.Context) {
 			}
 		}
 	}()
-
-	/*
-		go func() {
-			err := d.synchronizeServers()
-			if err != nil {
-				bslog.Error("failed to synchronize servers", slog.String("reason", err.Error()))
-			}
-
-			for {
-				select {
-				case <-ctx.Done():
-					bslog.Info("stopping dnsdist - server synchronization")
-
-					// close controll socket connections
-					for _, server := range d.servers {
-						if server.client != nil {
-							bslog.Debug("closing dnsdist - server connection", slog.String("server", server.name))
-							server.client.Close()
-						}
-					}
-
-					return
-				case <-time.After(DEFAULT_SYNCHRONIZE_JOB):
-					bslog.Debug("starting dnsdist server synchronization")
-					err := d.synchronizeServers()
-					if err != nil {
-						bslog.Error("unable to synchronize dnsdist - servers", slog.String("reason", err.Error()))
-					}
-				}
-			}
-		}()
-	*/
 }
 
 func (d *DNSDISTUpdater) synchronizeServers() error {
@@ -248,7 +207,7 @@ func (d *DNSDISTUpdater) synchronizeServers() error {
 	hashesByView := make(map[string]string)
 	for _, server := range d.servers {
 		wg.Go(func() error {
-			hash, ok := hashesByView[""]
+			hash, ok := hashesByView[server.selector.View()]
 			if !ok {
 				viewHash, err := d.spoofRepo.Hash(server.selector.View())
 				if err != nil {
@@ -258,9 +217,19 @@ func (d *DNSDISTUpdater) synchronizeServers() error {
 				hash = viewHash
 			}
 
-			serverHash, err := server.Hash()
+			var serverHash string
+			err := d.do(server.name,
+				func() error {
+					hash, err := server.Hash()
+					if err != nil {
+						return err
+					}
+					serverHash = hash
+					return nil
+				})
+
 			if err != nil {
-				return fmt.Errorf("failed to fetch %s hash: %w", server.name, err)
+				return fmt.Errorf("failed to compute %s hash: %w", server.name, err)
 			}
 
 			if hash != serverHash {
@@ -269,7 +238,11 @@ func (d *DNSDISTUpdater) synchronizeServers() error {
 					return fmt.Errorf("failed to read spoofs: %w", err)
 				}
 
-				return server.Reconcile(gslbSpoofs)
+				err = d.do(server.name, func() error { return server.Reconcile(gslbSpoofs) })
+				if err != nil {
+					return fmt.Errorf("failed to reconcile %s: %w", server.name, err)
+				}
+				return nil
 			}
 
 			return nil
@@ -282,146 +255,3 @@ func (d *DNSDISTUpdater) synchronizeServers() error {
 
 	return nil
 }
-
-/*
-func (d *DNSDISTUpdater) synchronizeServers() error {
-	desiredHash, err := d.spoofRepo.Hash()
-	if err != nil {
-		return fmt.Errorf("unable to get hash representation of spoofs: %w", err)
-	}
-
-	wg := sync.WaitGroup{}
-	syncErrors := make(chan error, len(d.servers))
-
-	for _, srv := range d.servers {
-		wg.Add(1)
-		go func(server *server) {
-			defer wg.Done()
-
-			var rawRuleSet string
-			err := d.do(server.name, func() error {
-				var err error
-				rawRuleSet, err = server.client.Rules().List(&rules.ListOptions{ShowUUIDs: new(true)})
-				return err
-			})
-
-			if err != nil {
-				bslog.Error("unable to fetch ruleset from dnsdist server", slog.String("reason", err.Error()))
-				syncErrors <- fmt.Errorf("synchronization of %s failed: %w", server.name, err)
-				return
-			}
-
-			spoofUUIDs, err := d.ParseRuleSet(rawRuleSet)
-			if err != nil {
-				bslog.Error("failed to parse dnsdist rule set", slog.String("reason", err.Error()), "server_name", server)
-				syncErrors <- err
-				return
-			}
-
-			joinedUUIDs := strings.Join(spoofUUIDs, ",")
-			rawHash := sha256.Sum256([]byte(joinedUUIDs))
-			hash := hex.EncodeToString(rawHash[:])
-
-			if hash != desiredHash {
-				err := d.reconcileServer(server.client, spoofUUIDs)
-				if err != nil {
-					bslog.Error("failed to reconcile dnsdist server", slog.String("server_name", server.name), slog.String("reason", err.Error()))
-					syncErrors <- err
-					events.Emit(&events.Event{
-						Type:      domainEvents.EventTypeDNSDISTServerOutOfSync,
-						Payload:   domainEvents.DNSDistServerOutOfSyncEvent{},
-						Timestamp: time.Now(),
-						ID:        events.ID(domainEvents.EventTypeDNSDISTServerOutOfSync, server.name),
-					})
-					return
-				}
-			}
-		}(srv)
-	}
-
-	wg.Wait()
-	close(syncErrors)
-
-	for err := range syncErrors {
-		if err != nil {
-			events.Emit(&events.Event{
-				Type: domainEvents.EventTypeDNSDISTSyncFailed,
-				Payload: domainEvents.DNSDistSyncFailedEvent{
-					Reason: err.Error(),
-				},
-				Timestamp: time.Now(), // TODO: what should the subject in the ID be?
-				ID:        events.ID(domainEvents.EventTypeDNSDISTSyncFailed, "<server>?"),
-			})
-			return err
-		}
-	}
-
-	return nil
-}
-
-func (d *DNSDISTUpdater) ParseRuleSet(ruleSet string) ([]string, error) {
-	reader := strings.NewReader(ruleSet)
-	lines := bufio.NewScanner(reader)
-
-	pattern, err := regexp.Compile(`[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}|qname==[a-zA-Z0-9-_\.]+|spoof`)
-	if err != nil {
-		return nil, fmt.Errorf("unable to compile regex: %w", err)
-	}
-
-	spoofRules := make([]string, 0)
-	for lines.Scan() {
-		line := lines.Text()
-
-		matches := pattern.FindAllString(line, -1)
-
-		if len(matches) < 3 {
-			continue
-		}
-
-		rule := rules.RuleLine{
-			UUID:   matches[0],
-			Rule:   matches[1],
-			Action: matches[2],
-		}
-
-		if rule.Action != "spoof" && !strings.Contains(rule.Rule, "qname") {
-			continue
-		}
-
-		spoofRules = append(spoofRules, rule.UUID)
-	}
-	slices.Sort(spoofRules)
-
-	return spoofRules, nil
-}
-
-func (d *DNSDISTUpdater) reconcileServer(client dnsdist.Client, configuredSpoofUUIDs []string) error {
-	gslbspoofs, err := d.spoofRepo.ReadAll()
-	if err != nil {
-		return fmt.Errorf("could not fetch spoofs: %w", err)
-	}
-
-	for _, spoof := range configuredSpoofUUIDs { // remove all spoofs that should not exist any more
-		err := client.Rules().Remove(spoof)
-		if err != nil {
-			return fmt.Errorf("failed to remove configured spoofs: %w", err)
-		}
-	}
-
-	for _, spoof := range gslbspoofs { // add all spoofs that does not exist but should
-		err := client.Rules().Add(
-			rules.QNameRule(spoof.FQDN),
-			rules.SpoofAction(spoof.Address.Strings(), rules.SpoofActionOptions{TTL: new(30)}),
-			rules.GlobalRuleOptions{
-				Name: &spoof.Name,
-				UUID: &spoof.UUID,
-			},
-		)
-		if err != nil {
-			return fmt.Errorf("failed to add spoofs: %w", err)
-		}
-	}
-
-	return nil
-}
-*/
