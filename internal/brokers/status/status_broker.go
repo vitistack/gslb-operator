dedc3b702ea55fdaaae19b0bc073502e3e593b19
Espen Wobbes <espen.wobbes@nhn.no>
2026-09-09 14:03:57 +0200

    generic scheduler for batch status, will refactor manager to use the generic scheduler aswell

--- internal/brokers/status/status_broker.go
+++ internal/brokers/status/status_broker.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"log/slog"
 	"slices"
+	"sync"
 	"time"
 
 	"github.com/vitistack/gslb-operator/internal/config"
+	"github.com/vitistack/gslb-operator/internal/dns"
+	"github.com/vitistack/gslb-operator/internal/model"
 	domainEvents "github.com/vitistack/gslb-operator/internal/model/events"
 	"github.com/vitistack/gslb-operator/internal/repositories/servicegroup"
 	"github.com/vitistack/gslb-operator/internal/repositories/status"
@@ -17,22 +20,26 @@ import (
 	serviceModels "github.com/vitistack/gslb-operator/pkg/models/service"
 	"github.com/vitistack/gslb-operator/pkg/mq"
 	"github.com/vitistack/gslb-operator/pkg/mq/rabbitmq"
+	"github.com/vitistack/gslb-operator/pkg/scheduler"
 )
 
 // periodically
 type StatusBroker struct {
 	client           mq.MessageBroker[serviceModels.SiteGSLBServiceStatus]
+	dnsStatusFetcher dns.StatusFetcher
 	statusRepo       *status.StatusRepo
 	serviceGroupRepo *servicegroup.ServiceGroupRepo
+	scheduler        *scheduler.Scheduler[string]
+	wg               *sync.WaitGroup
 }
 
-func Init(ctx context.Context, statusRepo *status.StatusRepo, groupRepo *servicegroup.ServiceGroupRepo) {
+func Init(ctx context.Context, statusRepo *status.StatusRepo, groupRepo *servicegroup.ServiceGroupRepo, dnsStatusFetcher dns.StatusFetcher) {
 	if config.GSLB().StatusEnabled() {
-		NewStatusBroker(ctx, statusRepo, groupRepo).Subscribe(ctx)
+		NewStatusBroker(ctx, statusRepo, groupRepo, dnsStatusFetcher).Subscribe(ctx)
 	}
 }
 
-func NewStatusBroker(ctx context.Context, statusRepo *status.StatusRepo, groupRepo *servicegroup.ServiceGroupRepo) *StatusBroker {
+func NewStatusBroker(ctx context.Context, statusRepo *status.StatusRepo, groupRepo *servicegroup.ServiceGroupRepo, dnsStatusFetcher dns.StatusFetcher) *StatusBroker {
 	mqCfg := config.MQ()
 	var amqpUrlPrefix string
 
@@ -42,10 +49,15 @@ func NewStatusBroker(ctx context.Context, statusRepo *status.StatusRepo, groupRe
 	default:
 		amqpUrlPrefix = "amqps://"
 	}
+	wg := sync.WaitGroup{}
+	scheduler := scheduler.NewScheduler[string](time.Minute*5, &wg)
 
 	broker := &StatusBroker{
 		statusRepo:       statusRepo,
 		serviceGroupRepo: groupRepo,
+		dnsStatusFetcher: dnsStatusFetcher,
+		scheduler:        &scheduler,
+		wg:               &wg,
 		client: rabbitmq.New(
 			ctx,
 			fmt.Sprintf(
@@ -60,11 +72,55 @@ func NewStatusBroker(ctx context.Context, statusRepo *status.StatusRepo, groupRe
 			rabbitmq.WithFanout[serviceModels.SiteGSLBServiceStatus](),
 		),
 	}
+	broker.scheduler.OnTick(func(memberOf string) {
+		go func() {
+			broker.PublishStatusForGroup(memberOf, time.Now())
+		}()
+	})
 
 	events.On(domainEvents.EventTypeGSLBService, broker)
 	broker.Subscribe(ctx)
 
+	go broker.coldStart()
+	go func() {
+		<-ctx.Done()
+		broker.scheduler.Stop()
+	}()
+
 	return broker
+}
+
+func (s *StatusBroker) coldStart() {
+	counter := 0
+	groups, finish := s.serviceGroupRepo.ReadAll()
+
+	groups.Each(
+		func(group model.GSLBServiceGroup) {
+			var memberOf string
+			for _, member := range group.Members {
+				memberOf = member.MemberOf
+				break
+			}
+
+			if memberOf != "" {
+				counter++
+				bslog.Debug("scheduling regular service status publishing", slog.String("memberOf", memberOf))
+				s.scheduler.Schedule(memberOf)
+			}
+		},
+	)
+
+	bslog.Debug("scheduled regular service status updates", slog.Int("servicesTotal", counter))
+	if err := finish(); err != nil {
+		bslog.Error("failed to read service groups for status scheduling", slog.String("reason", err.Error()))
+	}
+}
+
+func (s *StatusBroker) reconcileScheduledGroup(memberOf string) {
+	group, err := s.serviceGroupRepo.Read(memberOf)
+	if err != nil || len(group.Members) == 0 {
+		s.scheduler.Remove(func(m string) bool { return m == memberOf })
+	}
 }
 
 func (s *StatusBroker) Publish(ctx context.Context, siteStatus serviceModels.SiteGSLBServiceStatus) error {
@@ -113,46 +169,25 @@ func (s *StatusBroker) Handle(e *events.Event) {
 		return
 	}
 
-	bslog.Debug("handling status-update event")
-	siteStatus := service.SiteGSLBServiceStatus{
-		Service: event.GetMemberOf(),
-		Site:    config.GSLB().Site(),
-		LocalGSLBServiceStatus: serviceModels.LocalGSLBServiceStatus{
-			LastSeen: e.Timestamp,
-			Members:  make([]serviceModels.ShortGSLBServiceMemberStatus, 0),
-		},
+	bslog.Debug("handling status-update event", slog.String("eventType", fmt.Sprintf("%T", e.Payload)))
+
+	switch e.Payload.(type) {
+	case domainEvents.GSLBServiceMemberAddEvent, domainEvents.GSLBServiceMemberRemoveEvent:
+		s.reconcileScheduledGroup(event.GetMemberOf())
 	}
 
-	serviceGroup, err := s.serviceGroupRepo.Read(event.GetMemberOf())
-	if err != nil {
+	if err := s.PublishStatusForGroup(event.GetMemberOf(), e.Timestamp); err != nil {
 		bslog.Error(
 			"failed to publish gslb-site status for "+event.GetMemberOf(),
 			slog.String("reason", err.Error()),
-		)
-	}
-
-	if len(serviceGroup.Active) == 0 {
-		siteStatus.Healthy = false
-	} else {
-		siteStatus.Healthy = true
-	}
-
-	for _, member := range serviceGroup.Members {
-		siteStatus.LocalGSLBServiceStatus.Members = append(siteStatus.LocalGSLBServiceStatus.Members, member.GSLBServiceMemberStatus())
-	}
-
-	err = s.Publish(context.Background(), siteStatus)
-	if err != nil {
-		bslog.Error(
-			"failed to publish gslb-site status for "+event.GetMemberOf(),
-			slog.String("reason", err.Error()),
+			slog.String("event_id", e.ID),
 		)
 	}
 }
 
 // interface satisfaction for EventHandler on internal events.Emit(...)
 func (s *StatusBroker) GetID() string {
-	return ""
+	return "status:broker"
 }
 
 func (s *StatusBroker) handleSiteStatus(ctx context.Context, status serviceModels.SiteGSLBServiceStatus) error {
@@ -179,4 +214,46 @@ func (s *StatusBroker) handleSiteStatus(ctx context.Context, status serviceModel
 	gslbStatus.Sites[idx] = status
 
 	return s.statusRepo.Update(status.Service, gslbStatus)
+}
+
+func (s *StatusBroker) PublishStatusForGroup(group string, lastSeen time.Time) error {
+	siteStatus := service.SiteGSLBServiceStatus{
+		Service:  group,
+		Site:     config.GSLB().Site(),
+		LastSeen: lastSeen,
+		Members:  make([]serviceModels.ShortGSLBServiceMemberStatus, 0),
+	}
+
+	serviceGroup, err := s.serviceGroupRepo.Read(group)
+	if err != nil {
+		return err
+	}
+
+	siteStatus.Health = service.HEALTHY
+	numUnhealthy := 0
+
+	for _, member := range serviceGroup.Members {
+		if !member.IsHealthy {
+			siteStatus.Health = service.DEGRADED
+			numUnhealthy++
+		}
+		siteStatus.LocalGSLBServiceStatus.Members = append(siteStatus.LocalGSLBServiceStatus.Members, member.GSLBServiceMemberStatus())
+	}
+
+	if numUnhealthy >= len(siteStatus.Members) {
+		siteStatus.Health = service.DOWN
+	}
+
+	dnsStatus, err := s.dnsStatusFetcher.FetchStatus(serviceGroup.UUID.String())
+	if err != nil {
+		return err
+	}
+	siteStatus.DNSStatus = dnsStatus
+
+	err = s.Publish(context.Background(), siteStatus)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
---
+++ pkg/scheduler/scheduler.go
@@ -0,0 +1,218 @@
+package scheduler
+
+import (
+	"container/heap"
+	"log/slog"
+	"math/rand/v2"
+	"sync"
+	"time"
+
+	"github.com/vitistack/gslb-operator/pkg/bslog"
+)
+
+const OFFSETS_PER_SECOND = 2
+const OFFSET = time.Second / OFFSETS_PER_SECOND
+
+// wrapper for service which is scheduled on the heap
+type ScheduledItem[T any] struct {
+	item          T
+	nextCheckTime time.Time
+	offsett       time.Duration
+
+	// only used when RemoveService wants to remove the service
+	// that is currently at the top of the heap
+	shouldReSchedule bool
+}
+
+type Scheduler[T any] struct {
+	// base-interval that the service distributes services on
+	interval time.Duration
+
+	// maximum number of offsets possible for the interval
+	maxOffSets int
+
+	// next offsett to schedule a service on
+	nextOffset int
+
+	// queue were the next scheduled service is on top of the heap
+	heap ItemHeap[T]
+
+	// random jitter to spread out scheduled service on interval and sub-tick
+	jitterRange time.Duration
+
+	stop chan struct{} // signal stop
+	wg   *sync.WaitGroup
+	mu   sync.Mutex
+
+	isRunning bool
+
+	// action to take when ScheduledService.nectCheckTime has been reached
+	onTick func(T)
+}
+
+func NewScheduler[T any](interval time.Duration, wg *sync.WaitGroup) Scheduler[T] {
+	h := make(ItemHeap[T], 0)
+
+	maxOffsets := int(interval.Seconds() * 2)
+
+	return Scheduler[T]{
+		interval:    interval,
+		maxOffSets:  maxOffsets,
+		nextOffset:  0,
+		heap:        h,
+		jitterRange: time.Duration(float64(interval) * 0.1),
+		stop:        make(chan struct{}),
+		wg:          wg,
+		mu:          sync.Mutex{},
+		isRunning:   false,
+	}
+}
+
+func (s *Scheduler[T]) Stop() {
+	close(s.stop)
+}
+
+func (s *Scheduler[T]) OnTick(fn func(T)) {
+	s.onTick = fn
+}
+
+// schedule a new service on the heap
+func (s *Scheduler[T]) Schedule(item T) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	offset := OFFSET * time.Duration(s.nextOffset)
+	scheduled := ScheduledItem[T]{
+		item:             item,
+		nextCheckTime:    time.Now().Add(offset).Add(s.newTickInterval()),
+		shouldReSchedule: true,
+	}
+
+	s.nextOffset = (s.nextOffset + 1) % s.maxOffSets
+
+	heap.Push(&s.heap, &scheduled)
+	if s.heap.Len() == 1 {
+		s.startLoop() // restart the loop when we have scheduled services in the heap
+	}
+}
+
+// cmp is a function used to compare scheduled items if it exists or not.
+func (s *Scheduler[T]) Remove(cmp func(T) bool) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	idx := s.heap.GetIndex(
+		func(scheduled *ScheduledItem[T]) bool {
+			return cmp(scheduled.item)
+		},
+	)
+
+	switch idx {
+	case -1:
+		return s.heap.Len() == 0
+	case 0:
+		s.heap[0].shouldReSchedule = false
+		if len(s.heap) == 1 {
+			return true
+		}
+	default:
+		heap.Remove(&s.heap, idx)
+	}
+
+	return s.heap.Len() == 0
+}
+
+// re-schedule the service at the top of the heap
+func (s *Scheduler[T]) reSchedule() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	top := heap.Pop(&s.heap).(*ScheduledItem[T])
+
+	if !top.shouldReSchedule {
+		// just return since the service is now removed from the heap
+		return
+	}
+
+	top.nextCheckTime = time.Now().Add(s.newTickInterval()) // initiate the next checktime
+
+	heap.Push(&s.heap, top)
+
+	if s.heap.Len() == 0 {
+		s.startLoop()
+	}
+}
+
+func (s *Scheduler[T]) startLoop() {
+	if s.isRunning {
+		return
+	}
+	s.isRunning = true
+	s.loop()
+}
+
+func (s *Scheduler[T]) loop() {
+	s.wg.Go(func() {
+		defer func() {
+			s.mu.Lock()
+			s.isRunning = false
+			s.mu.Unlock()
+			bslog.Debug("scheduler closed", slog.String("interval", s.interval.String()))
+		}()
+
+		for {
+			select {
+			case <-s.stop: // check stop
+				bslog.Debug("got stop, exiting scheduler...")
+				return
+			default:
+			}
+
+			s.mu.Lock()
+			if s.heap.Len() == 0 { // no need to infinitly run on an empty queue
+				s.mu.Unlock()
+				return
+			}
+
+			next := s.heap.Peek()
+			s.mu.Unlock()
+			if next.nextCheckTime.Before(time.Now()) { // check time already past, do action immediately and reschedule
+				if s.onTick != nil {
+					s.onTick(next.item)
+				}
+
+				select {
+				case <-s.stop: // check stop
+					bslog.Debug("got stop, exiting scheduler...")
+					return
+				default:
+				}
+
+				s.reSchedule()
+			} else {
+				timeUntil := time.Until(next.nextCheckTime)
+				select {
+				case <-s.stop:
+					bslog.Debug("got stop, exiting scheduler...")
+					return
+				case <-time.After(timeUntil):
+					if s.onTick != nil {
+						s.onTick(next.item)
+					}
+
+					select {
+					case <-s.stop: // check stop
+						bslog.Debug("got stop, exiting scheduler...")
+						return
+					default:
+					}
+
+					s.reSchedule()
+				}
+			}
+		}
+	})
+}
+
+func (s *Scheduler[T]) newTickInterval() time.Duration {
+	jitter := time.Duration((rand.Float64()*2 - 1) * float64(s.jitterRange)) // get a random jitter inside the jitter range
+	return (s.interval + jitter).Round(time.Second / 10)                     // create the interval duration, rounded to the nearest 10th of a second
+}
